fcd9e892539122caf958ab1fcbf469cc4dcc6e8e
ritu-477 <ritujoharbishnoi@gmail.com>
2024-03-21 12:27:17 +0530

    our-experince section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,55 @@
-const iconbox = document .querySelector(".iconbox")
+const iconbox = document.querySelector(".iconbox")
 const menulist = document.querySelector(".menulist")
 
 iconbox.addEventListener("click", function () {
     menulist.classList.toggle("show");
     document.body.classList.toggle("overflow-hidden");
+});
+$('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    autoplay: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    nextArrow: ".arrowbtnprev",
+    prevArrow: ".arrowbtnnext",
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+
+            }
+        },
+
+    ]
 });
