6bef421503c74954af75578faa2425af7cf1ea05
ritu-477 <ritujoharbishnoi@gmail.com>
2024-03-21 21:56:52 +0530

    clients-feedback slider-section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,51 @@ $('.responsive').slick({
 
     ]
 });
+$('.responsive2').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    autoplay: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    nextArrow: "",
+    prevArrow: "",
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+
+            }
+        },
+
+    ]
+});
