e93a4755944cb985c96f3f88a4fd8a8069597f6a
ritu-477 <ritujoharbishnoi@gmail.com>
2024-03-22 14:38:54 +0530

    footer-section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,21 @@ iconbox.addEventListener("click", function () {
     menulist.classList.toggle("show");
     document.body.classList.toggle("overflow-hidden");
 });
+const mybutton = document.querySelector(".elipsBtn");
+const show = document.querySelector(".show")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        mybutton.classList.add("show")
+    }
+    else {
+        mybutton.classList.remove("show")
+    }
+})
+mybutton.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+
+});
 $('.responsive').slick({
     dots: true,
     infinite: true,
